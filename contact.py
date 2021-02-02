828b799154058ef439c983056853e3af423bd7b0
senor-green-hill <juanabarro@protonmail.com>
2021-02-02 15:26:47 -0500

    updates to contact.py

--- contact.py
+++ contact.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+
 
 class ContactForm(forms.Form):
     yourname = forms.CharField(max_length = 100, label = 'Your Name')
@@ -14,7 +16,13 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            assert False
+            # assert False
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'), ['siteowner@example.com'], connection = con
+            )
             return HttpResponseRedirect('/contact?submitted=True')
     else:
         form = ContactForm()
